f94822efe49659ecb7762514f97ba215b644253c
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-10-31 17:06:40 +0100

    add tmp save and repeat on emb generation

--- usrembeds/embedgen.py
+++ usrembeds/embedgen.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     stats_path = "../scraper/data/clean_stats.csv"
     emb_path = "usrembeds/data/embeddings"
 
-    SAVE_RATE = 500
+    SAVE_RATE = 1000
 
     BATCH_SIZE = 32
     EMB_SIZE = 512
@@ -32,6 +32,8 @@ if __name__ == "__main__":
         stats_path,
         audio_len=AUDIO_LEN,
         resample=TARGET_SR,
+        # nsamples=50,
+        repeat=3,
     )
     dataloader = torch.utils.data.DataLoader(
         dataset,
@@ -50,38 +52,41 @@ if __name__ == "__main__":
     }
 
     for idx, track in enumerate(dataloader):
-        if idx >= 1224:
-            start_time = time.time()
+        start_time = time.time()
 
-            stat, audio = track
+        stat, audio = track
 
-            sr = stat["sr"][0].item()
+        sr = stat["sr"][0].item()
 
-            audio = audio.to(DEVICE)
-            emb, ts = torchopenl3.get_audio_embedding(
-                audio,
-                sr,
-                model=model,
-                hop_size=HOP_SIZE,
-                embedding_size=EMB_SIZE,
-            )
+        audio = audio.to(DEVICE)
+        emb, ts = torchopenl3.get_audio_embedding(
+            audio,
+            sr,
+            model=model,
+            hop_size=HOP_SIZE,
+            embedding_size=EMB_SIZE,
+        )
 
-            # print(emb.shape)
-            # plot_music_batch(emb, DEVICE)
-            mean_emb = emb.mean(axis=1)
+        # print(emb.shape)
+        # plot_music_batch(emb, DEVICE)
+        mean_emb = emb.mean(axis=1)
 
-            for i, track_id in enumerate(stat["id"]):
-                if track_id not in emb_dict:
-                    emb_dict[track_id] = []
-                emb_dict[track_id].append(mean_emb[i].cpu().detach().tolist())
+        for i, track_id in enumerate(stat["id"]):
+            if track_id not in emb_dict:
+                emb_dict[track_id] = []
+            emb_dict[track_id].append(mean_emb[i].cpu().detach().tolist())
 
-            if idx % SAVE_RATE == 0:
-                with open(os.path.join(emb_path, f"embeddings_{idx}.json"), "w") as f:
-                    json.dump(emb_dict, f)
-            end_time = time.time()
-            # print(f"{(end_time - start_time)/BATCH_SIZE} seconds per track")
+        if idx % SAVE_RATE == 0:
+            with open(os.path.join(emb_path, f"embeddings_{idx}.json"), "w") as f:
+                json.dump(emb_dict, f)
+
+        with open(os.path.join(emb_path, f"embeddings_chkp.json"), "w") as f:
+            json.dump(emb_dict, f)
+
+        end_time = time.time()
+        # print(f"{(end_time - start_time)/BATCH_SIZE} seconds per track")
 
         print(f"{idx}/{len(dataloader)}")
-
+    # breakpoint()
     with open(os.path.join(emb_path, f"embeddings_last.json"), "w") as f:
         json.dump(emb_dict, f)
