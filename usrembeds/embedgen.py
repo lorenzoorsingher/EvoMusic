4207b7292614e4766c17aa5f6303e422fa105ad0
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-10-31 14:54:38 +0100

    fix music embeddings gen

--- usrembeds/embedgen.py
+++ usrembeds/embedgen.py
@@ -14,14 +14,15 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"  # use GPU if we can!
 
 if __name__ == "__main__":
 
-    music_path = "/home/lollo/Documents/python/BIO/project/scraper/music"
-    stats_path = "/home/lollo/Documents/python/BIO/project/scraper/data/clean_stats.csv"
-    emb_path = "data/embeddings"
+    music_path = "../scraper/music"
+    stats_path = "../scraper/data/clean_stats.csv"
+    emb_path = "usrembeds/data/embeddings"
 
-    SAVE_RATE = 10
+    SAVE_RATE = 500
 
     BATCH_SIZE = 32
     EMB_SIZE = 512
+
     HOP_SIZE = 0.1
     AUDIO_LEN = 3
     TARGET_SR = torchopenl3.core.TARGET_SR
@@ -29,7 +30,6 @@ if __name__ == "__main__":
     dataset = MusicDataset(
         music_path,
         stats_path,
-        type="audio",
         audio_len=AUDIO_LEN,
         resample=TARGET_SR,
     )
@@ -45,36 +45,43 @@ if __name__ == "__main__":
         embedding_size=EMB_SIZE,
     )
 
-    emb_dict = {}
+    emb_dict = {
+        "metadata": {"hop_size": HOP_SIZE, "audio_len": AUDIO_LEN, "sr": TARGET_SR}
+    }
 
     for idx, track in enumerate(dataloader):
-        start_time = time.time()
+        if idx >= 1224:
+            start_time = time.time()
 
-        stat, audio = track
+            stat, audio = track
 
-        sr = stat["sr"][0].item()
+            sr = stat["sr"][0].item()
 
-        audio = audio.to(DEVICE)
-        emb, ts = torchopenl3.get_audio_embedding(
-            audio,
-            sr,
-            model=model,
-            hop_size=HOP_SIZE,
-            embedding_size=EMB_SIZE,
-        )
+            audio = audio.to(DEVICE)
+            emb, ts = torchopenl3.get_audio_embedding(
+                audio,
+                sr,
+                model=model,
+                hop_size=HOP_SIZE,
+                embedding_size=EMB_SIZE,
+            )
 
-        print(emb.shape)
+            # print(emb.shape)
+            # plot_music_batch(emb, DEVICE)
+            mean_emb = emb.mean(axis=1)
 
-        # plot_music_batch(emb, DEVICE)
-        mean_emb = emb.mean(axis=1)
+            for i, track_id in enumerate(stat["id"]):
+                if track_id not in emb_dict:
+                    emb_dict[track_id] = []
+                emb_dict[track_id].append(mean_emb[i].cpu().detach().tolist())
 
-        for i, track_id in enumerate(stat["id"]):
-            if track_id not in emb_dict:
-                emb_dict[track_id] = []
-            emb_dict[track_id].append(mean_emb[i].cpu().detach().tolist())
+            if idx % SAVE_RATE == 0:
+                with open(os.path.join(emb_path, f"embeddings_{idx}.json"), "w") as f:
+                    json.dump(emb_dict, f)
+            end_time = time.time()
+            # print(f"{(end_time - start_time)/BATCH_SIZE} seconds per track")
 
-        if idx % SAVE_RATE == 0:
-            with open(os.path.join(emb_path, f"embeddings_{idx}.json"), "w") as f:
-                json.dump(emb_dict, f)
-        end_time = time.time()
-        print(f"{(end_time - start_time)/BATCH_SIZE} seconds per track")
+        print(f"{idx}/{len(dataloader)}")
+
+    with open(os.path.join(emb_path, f"embeddings_last.json"), "w") as f:
+        json.dump(emb_dict, f)
