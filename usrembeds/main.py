f7c866ade604f13df99752a8553b35895fb6e421
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-10-31 14:54:48 +0100

    add training loop

--- usrembeds/main.py
+++ usrembeds/main.py
@@ -2,61 +2,79 @@ import torchopenl3
 import torch
 import numpy as np
 
-from datautils.dataset import MusicDataset
+from datautils.dataset import MusicDataset, StatsDataset, ContrDataset
+from models.model import Aligner
 from utils import plot_music_batch
+import torch.nn as nn
 
+from torch import optim
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"  # use GPU if we can!
 
-# torchopenl3.ge
 
-# model = torchopenl3.OpenL3Embedding(
-#     input_repr="mel128", embedding_size=512, content_type="music"
-# )
+def constrstive_loss(possim, negsim, temp=0.07):
+    # breakpoint()
+
+    possim = cos(out, posemb)
+
+    out = out.repeat(1, negemb.shape[1], 1)
+    negsim = cos(out, negemb)
+
+    logits = torch.cat((possim, negsim), dim=1) / TEMP
+    exp = torch.exp(logits)
+    loss = -torch.log(exp[:, 0] / torch.sum(exp, dim=1))
+    loss = torch.mean(loss)
 
 
 if __name__ == "__main__":
-
-    music_path = "/home/lollo/Documents/python/BIO/project/scraper/music"
-    stats_path = "/home/lollo/Documents/python/BIO/project/scraper/data/clean_stats.csv"
 
     BATCH_SIZE = 16
     EMB_SIZE = 512
     HOP_SIZE = 0.2
     AUDIO_LEN = 5
 
-    # mel_cfg = {
-    #     "n_mels": 128,
-    #     "hop_length": HOP_SIZE,
-    #     "audio_len": AUDIO_LEN,
-    # }
+    music_path = "../scraper/music"
+    membs_path = "usrembeds/data/embeddings/embeddings_1000.json"
+    stats_path = "../scraper/data/clean_stats.csv"
 
-    dataset = MusicDataset(
-        music_path,
-        stats_path,
-        type="audio",
-        audio_len=AUDIO_LEN,
-    )
-    dataloader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=BATCH_SIZE,
-        shuffle=True,
-    )
+    dataset = ContrDataset(membs_path, stats_path, nneg=32, transform=None)
+    NUSERS = dataset.nusers
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
-    for track in dataloader:
-        stat, audio = track
+    model = Aligner(NUSERS, EMB_SIZE).to(DEVICE)
 
-        sr = stat["sr"][0].item()
+    EPOCHS = 10
 
-        emb, ts = torchopenl3.get_audio_embedding(
-            audio,
-            sr,
-            hop_size=HOP_SIZE,
-            embedding_size=EMB_SIZE,
-        )
+    cos = nn.CosineSimilarity(dim=2, eps=1e-6)
 
-        print(emb.shape)
+    opt = optim.Adam(model.parameters(), lr=0.001)
+    TEMP = 0.07
 
-        plot_music_batch(emb, DEVICE)
+    for epoch in range(EPOCHS):
 
-        breakpoint()
+        print(f"Epoch {epoch}")
+        losses = []
+        for tracks in dataloader:
+
+            # [B]
+            # [B, 1, EMB]
+            # [B, NNEG, EMB]
+            idx, posemb, negemb = tracks
+
+            idx = idx.to(DEVICE)
+            posemb = posemb.to(DEVICE)
+            negemb = negemb.to(DEVICE)
+            opt.zero_grad()
+            out = model(idx)
+
+            out = out.unsqueeze(1)
+
+            loss = constrstive_loss(out, posemb, negemb)
+
+            losses.append(loss.item())
+
+            loss.backward()
+            opt.step()
+
+        print(np.mean(losses))
+        # print(loss)
