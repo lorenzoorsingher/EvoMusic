c6cf1f51337276fa8699149451c7319222ee176d
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-11-07 15:50:08 +0100

    make user MLP deeper. Selectively instantiate params

--- usrembeds/models/model.py
+++ usrembeds/models/model.py
@@ -16,30 +16,31 @@ class Aligner(nn.Module):
         self.users = nn.Embedding(n_users, emb_size)
 
         self.fc1 = nn.Linear(emb_size, 4096)
+        self.fcmid = nn.Linear(4096, 4096)
         self.fc2 = nn.Linear(4096, prj_size)
-
-        self.ln = nn.LayerNorm(prj_size)
-
-        self.bn = nn.BatchNorm1d(prj_size)
-
-        self.ln2 = nn.LayerNorm(prj_size)
-        self.fc3 = nn.Linear(prj_size, 4096)
-        self.fc4 = nn.Linear(4096, prj_size)
 
         self.temp = temp
         if lt:
             print("[MODEL] Using learnable temperature")
             self.temp = nn.Parameter(torch.tensor(self.temp))
 
+        if self.prj_type == "linear":
+            self.ln2 = nn.LayerNorm(prj_size)
+            self.fc3 = nn.Linear(prj_size, 4096)
+            self.fc4 = nn.Linear(4096, prj_size)
+        elif self.prj_type == "ln":
+            self.ln = nn.LayerNorm(prj_size)
+        elif self.prj_type == "bn":
+            self.bn = nn.BatchNorm1d(prj_size)
+
         print(f"[MODEL] Using projection type: {prj_type}")
 
     def forward(self, idx, embs):
 
-        # idx, embs = x
-
         user_embs = self.users(idx)
 
         urs_x = F.gelu(self.fc1(user_embs))
+        urs_x = F.gelu(self.fcmid(urs_x))
         urs_x = self.fc2(urs_x)
 
         if self.prj_type == "linear":
@@ -49,7 +50,6 @@ class Aligner(nn.Module):
         elif self.prj_type == "ln":
             embs = self.ln(embs)
         elif self.prj_type == "bn":
-            # TODO: not sure this is the right way
             embs = embs.permute(0, 2, 1)
             embs = self.bn(embs)
             embs = embs.permute(0, 2, 1)
