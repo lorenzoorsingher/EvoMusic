21174a9b5a1a1025c0e28cd028fec1e3d98ce26b
Laurence Bonat <laurence.bonat@gmail.com>
2024-11-07 12:32:42 +0100

    fix: riffusion not working properly

--- music_generation/helpers_class.py
+++ music_generation/helpers_class.py
@@ -1,14 +1,19 @@
+import os
+from typing import Optional, Union
+
 import transformers
 import torch
+from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
 from torch import nn
-from diffusers import DiffusionPipeline
-from transformers import CLIPTokenizer, CLIPTextModel
+from diffusers import (DiffusionPipeline, AutoencoderKL, UNet2DConditionModel, DDIMScheduler, PNDMScheduler,
+                       LMSDiscreteScheduler)
+from transformers import CLIPTokenizer, CLIPTextModel, AutoTokenizer, CLIPFeatureExtractor
 
 RIFFUSION_MODEL_ID = "riffusion/riffusion-model-v1"
 MUSICGEN_MODEL_ID = "facebook/musicgen-small"
 
 
-class EasyDiffuse(nn.Module):
+class EasyDiffuse(DiffusionPipeline):
     def __init__(self):
         super(EasyDiffuse, self).__init__()
 
@@ -19,13 +24,27 @@ class EasyDiffuse(nn.Module):
         raise NotImplementedError
 
 
-class RiffusionHelpers(EasyDiffuse):
-    def __init__(self, device="cuda"):
+class RiffusionPipeline(EasyDiffuse):
+    def __init__(
+            self,
+            vae: AutoencoderKL,
+            text_encoder: CLIPTextModel,
+            tokenizer: CLIPTokenizer,
+            unet: UNet2DConditionModel,
+            scheduler: DDIMScheduler | PNDMScheduler | LMSDiscreteScheduler,
+            safety_checker: StableDiffusionSafetyChecker,
+            feature_extractor: CLIPFeatureExtractor,
+    ):
         super().__init__()
-        self.device = device
-        self.tokenizer = CLIPTokenizer.from_pretrained(RIFFUSION_MODEL_ID)
-        self.text_encoder = CLIPTextModel.from_pretrained(RIFFUSION_MODEL_ID)
-        self.model = DiffusionPipeline.from_pretrained(RIFFUSION_MODEL_ID)
+        self.register_modules(
+            vae=vae,
+            text_encoder=text_encoder,
+            tokenizer=tokenizer,
+            unet=unet,
+            scheduler=scheduler,
+            safety_checker=safety_checker,
+            feature_extractor=feature_extractor,
+        )
 
     def text_to_embed(self, text, max_length=None):
         if max_length is None:
@@ -48,10 +67,9 @@ class RiffusionHelpers(EasyDiffuse):
 
 
 class MusicGenHelpers(EasyDiffuse):
-    def __init__(self, device="cuda"):
+    def __init__(self):
         super().__init__()
-        self.device = device
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(MUSICGEN_MODEL_ID)
+        self.tokenizer = AutoTokenizer.from_pretrained(MUSICGEN_MODEL_ID)
         self.model = transformers.MusicgenForConditionalGeneration.from_pretrained(MUSICGEN_MODEL_ID)
 
     def text_to_embed(self, text, max_length=None):
@@ -67,9 +85,10 @@ class MusicGenHelpers(EasyDiffuse):
 
 
 if __name__ == "__main__":
-    dh = RiffusionHelpers()
-    dh.text_to_embed_musicgen("hello")
-    dh.text_to_embed_riffusion("hello")
-    dh.token_to_embed_riffusion("hello")
+    dh = RiffusionPipeline.from_pretrained(RIFFUSION_MODEL_ID)
+
+    a = dh.text_to_embed("hello world how is it going", 10)
+    b = dh.text_to_embeddings_before_clip("hello world how is it going", 10)
     breakpoint()
+
     print("done")
