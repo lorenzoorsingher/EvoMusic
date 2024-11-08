4388767433876134dcf95cf4e897360281f9e228
Laurence Bonat <laurence.bonat@gmail.com>
2024-11-08 15:04:09 +0100

    feat: added unit test for music generation

--- music_generation/helpers_class.py
+++ music_generation/helpers_class.py
@@ -1,3 +1,4 @@
+import math
 import os
 from typing import Optional, Union
 
@@ -7,7 +8,13 @@ from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
 from torch import nn
 from diffusers import (DiffusionPipeline, AutoencoderKL, UNet2DConditionModel, DDIMScheduler, PNDMScheduler,
                        LMSDiscreteScheduler)
-from transformers import CLIPTokenizer, CLIPTextModel, AutoTokenizer, CLIPFeatureExtractor
+from transformers import CLIPTokenizer, CLIPTextModel, AutoTokenizer, CLIPFeatureExtractor, \
+    MusicgenForConditionalGeneration
+
+from riffusion.spectrogram_image_converter import SpectrogramImageConverter
+from riffusion.spectrogram_params import SpectrogramParams
+from diffusers.utils.testing_utils import enable_full_determinism
+
 
 RIFFUSION_MODEL_ID = "riffusion/riffusion-model-v1"
 MUSICGEN_MODEL_ID = "facebook/musicgen-small"
@@ -61,13 +68,12 @@ class EasyRiffPipeline(EasyDiffuse):
 class MusicGenHelpers(EasyDiffuse):
     def __init__(self):
         super().__init__()
-        self.tokenizer = AutoTokenizer.from_pretrained(MUSICGEN_MODEL_ID)
         self.model = transformers.MusicgenForConditionalGeneration.from_pretrained(MUSICGEN_MODEL_ID)
 
     def text_to_embed(self, text, max_length=None):
         if max_length is None:
-            max_length = self.tokenizer.model_max_length
-        inputs = self.tokenizer(text, padding="max_length", max_length=max_length, truncation=True, return_tensors="pt")
+            max_length = self.model.tokenizer.model_max_length
+        inputs = self.model.tokenizer(text, padding="max_length", max_length=max_length, truncation=True, return_tensors="pt")
         with torch.no_grad():
             return self.model.get_encoder()(**inputs).last_hidden_state
 
@@ -77,10 +83,36 @@ class MusicGenHelpers(EasyDiffuse):
 
 
 if __name__ == "__main__":
-    dh = RiffusionPipeline.from_pretrained(RIFFUSION_MODEL_ID)
+    riffusion_pipe = EasyRiffPipeline()
+    enable_full_determinism()
+    generator = torch.Generator(device=DEVICE)
+    width = math.ceil(3 * (512 / 5))
+    width = width + (8 - width % 8) if width % 8 != 0 else width
+    generator.manual_seed(0)
+    embedding = riffusion_pipe.text_to_embed("Create a retro 80s synthwave track with nostalgic synthesizers, a steady electronic beat, and atmospheric reverb. Imagine a neon-lit night drive.", 30)
+    #output = dh(prompt_embeds=embedding_clip, num_inference_steps=50, width=width, generator=generator)
+    # image = output.images[0]
+    # embedding_pre = dh.text_to_embeddings_before_clip("It's Another tequila sunrise staring closely across the sky", 15)
+    # embedding_pre = dh.text_to_embeddings_before_clip("Create a retro 80s synthwave track with nostalgic synthesizers, a steady electronic beat, and atmospheric reverb. Imagine a neon-lit night drive.", 30)
+    # embedding = dh.token_embedding_to_embed(embedding_pre)
+    out2 = riffusion_pipe(prompt_embeds=embedding, num_inference_steps=50, width=width, generator=generator)
+    image = out2.images[0]
 
-    a = dh.text_to_embed("hello world how is it going", 10)
-    b = dh.text_to_embeddings_before_clip("hello world how is it going", 10)
-    breakpoint()
+    # Convert spectrogram image back to audio
+    params = SpectrogramParams()
+    converter = SpectrogramImageConverter(params=params)
+
+    segment = converter.audio_from_spectrogram_image(image, apply_filters=True, )
+
+    output_dir = "generated_audio"
+    os.makedirs(output_dir, exist_ok=True)
+    name = None
+    if name is None:
+        audio_filename = f"generated_music_{torch.randint(0, int(1e6), (1,)).item()}.wav"
+    else:
+        audio_filename = f"{name}.wav"
+    audio_path = os.path.join(output_dir, audio_filename)
+    segment.export(audio_path, format="wav")
+    # breakpoint()
 
     print("done")
