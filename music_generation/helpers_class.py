29560e948364a46df7c664bbab449b0b9e825fcc
Laurence Bonat <laurence.bonat@gmail.com>
2024-11-08 15:03:38 +0100

    feat: riffusion overhaul, correct token_embeddings output

--- music_generation/helpers_class.py
+++ music_generation/helpers_class.py
@@ -11,59 +11,51 @@ from transformers import CLIPTokenizer, CLIPTextModel, AutoTokenizer, CLIPFeatur
 
 RIFFUSION_MODEL_ID = "riffusion/riffusion-model-v1"
 MUSICGEN_MODEL_ID = "facebook/musicgen-small"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+def dummy_safety_checker(images, **kwargs):
+    return images, [False] * len(images)
 
 
-class EasyDiffuse(DiffusionPipeline):
+class EasyDiffuse:
     def __init__(self):
-        super(EasyDiffuse, self).__init__()
+        super().__init__()
 
     def text_to_embed(self, text):
         raise NotImplementedError
 
-    def token_to_embed(self, tokens):
+    def token_embedding_to_embed(self, token_embedding):
         raise NotImplementedError
 
 
-class RiffusionPipeline(EasyDiffuse):
-    def __init__(
-            self,
-            vae: AutoencoderKL,
-            text_encoder: CLIPTextModel,
-            tokenizer: CLIPTokenizer,
-            unet: UNet2DConditionModel,
-            scheduler: DDIMScheduler | PNDMScheduler | LMSDiscreteScheduler,
-            safety_checker: StableDiffusionSafetyChecker,
-            feature_extractor: CLIPFeatureExtractor,
-    ):
+class EasyRiffPipeline(EasyDiffuse):
+    def __init__(self):
         super().__init__()
-        self.register_modules(
-            vae=vae,
-            text_encoder=text_encoder,
-            tokenizer=tokenizer,
-            unet=unet,
-            scheduler=scheduler,
-            safety_checker=safety_checker,
-            feature_extractor=feature_extractor,
-        )
+        self.model = DiffusionPipeline.from_pretrained(RIFFUSION_MODEL_ID).to(DEVICE)
+
 
     def text_to_embed(self, text, max_length=None):
         if max_length is None:
-            max_length = self.tokenizer.model_max_length
-        inputs = self.tokenizer(text, padding="max_length", max_length=max_length, truncation=True, return_tensors="pt")
+            max_length = self.model.tokenizer.model_max_length
+        inputs = self.model.tokenizer(text, padding="max_length", max_length=max_length, truncation=True, return_tensors="pt")
         with torch.no_grad():
-            embedded_text = self.text_encoder(inputs.input_ids.to(self.device))
-        return embedded_text
+            embedded_text = self.model.text_encoder(inputs.input_ids.to(self.model.device))
+        return embedded_text.last_hidden_state
 
-    def token_to_embed(self, input_ids):
+    def token_embedding_to_embed(self, token_embedding):
         with torch.no_grad():
-            return self.text_encoder(input_ids.to(self.device))
+            encoded = self.model.text_encoder.text_model.encoder(token_embedding.to(self.model.device)).last_hidden_state
+            return self.model.text_encoder.text_model.final_layer_norm(encoded)
 
     def text_to_embeddings_before_clip(self, text, max_length=None):
         if max_length is None:
-            max_length = self.tokenizer.model_max_length
-        inputs = self.tokenizer(text, padding="max_length", max_length=max_length, truncation=True, return_tensors="pt")
+            max_length = self.model.tokenizer.model_max_length
+        inputs = self.model.tokenizer(text, padding="max_length", max_length=max_length, truncation=True, return_tensors="pt")
         with torch.no_grad():
-            return self.text_encoder.get_input_embeddings()(inputs.input_ids.to(self.device))
+            return self.model.text_encoder.text_model.embeddings(inputs.input_ids.to(self.model.device))
+
+    def __call__(self, *args, **kwargs):
+        return self.model(*args, **kwargs)
 
 
 class MusicGenHelpers(EasyDiffuse):
